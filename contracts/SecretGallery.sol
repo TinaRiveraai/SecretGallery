79762c38818dd04833f195a52210f0f18c63b591
TinaRiveraai <shbebsambas27@hotmail.com>
2025-09-18 13:20:36 +0800

    fix contracts

--- contracts/SecretGallery.sol
+++ contracts/SecretGallery.sol
@@ -8,27 +8,26 @@ import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 /// @notice A confidential gallery for storing encrypted IPFS hashes and AES passwords
 /// @dev Uses Zama FHE to store encrypted IPFS hashes (as numbers) and AES passwords (as addresses)
 contract SecretGallery is SepoliaConfig {
-    
     struct EncryptedFile {
-        euint256 ipfsHashNumber;  // IPFS hash converted to number, encrypted
-        eaddress aesPassword;     // AES password in EVM address format, encrypted
-        address owner;            // File owner
-        uint256 timestamp;        // Upload timestamp
-        bool exists;              // File existence flag
+        euint256 ipfsHashNumber; // IPFS hash converted to number, encrypted
+        eaddress aesPassword; // AES password in EVM address format, encrypted
+        address owner; // File owner
+        uint256 timestamp; // Upload timestamp
+        bool exists; // File existence flag
     }
 
     // File ID counter
     uint256 private _fileIdCounter;
-    
+
     // Mapping from file ID to encrypted file data
     mapping(uint256 => EncryptedFile) private _files;
-    
+
     // Mapping from owner to their file IDs
     mapping(address => uint256[]) private _ownerFiles;
-    
+
     // Mapping from file ID to granted addresses
     mapping(uint256 => mapping(address => bool)) private _fileGrants;
-    
+
     // Mapping from file ID to list of granted addresses (for enumeration)
     mapping(uint256 => address[]) private _fileGrantees;
 
@@ -47,14 +46,14 @@ contract SecretGallery is SepoliaConfig {
         externalEaddress encryptedPassword,
         bytes calldata inputProof
     ) external returns (uint256 fileId) {
-        // Validate and convert external inputs  
+        // Validate and convert external inputs
         euint256 ipfsHash = FHE.fromExternal(encryptedIpfsHash, inputProof);
         eaddress aesPassword = FHE.fromExternal(encryptedPassword, inputProof);
-        
+
         // Increment file ID counter
         _fileIdCounter++;
         fileId = _fileIdCounter;
-        
+
         // Store encrypted file data
         _files[fileId] = EncryptedFile({
             ipfsHashNumber: ipfsHash,
@@ -63,16 +62,16 @@ contract SecretGallery is SepoliaConfig {
             timestamp: block.timestamp,
             exists: true
         });
-        
+
         // Add to owner's file list
         _ownerFiles[msg.sender].push(fileId);
-        
+
         // Grant permissions
         FHE.allowThis(ipfsHash);
         FHE.allow(ipfsHash, msg.sender);
         FHE.allowThis(aesPassword);
         FHE.allow(aesPassword, msg.sender);
-        
+
         emit FileUploaded(fileId, msg.sender, block.timestamp);
     }
 
@@ -85,15 +84,15 @@ contract SecretGallery is SepoliaConfig {
         require(grantee != address(0), "Invalid grantee address");
         require(grantee != msg.sender, "Cannot grant to yourself");
         require(!_fileGrants[fileId][grantee], "Access already granted");
-        
+
         // Grant access
         _fileGrants[fileId][grantee] = true;
         _fileGrantees[fileId].push(grantee);
-        
+
         // Grant FHE permissions
         FHE.allow(_files[fileId].ipfsHashNumber, grantee);
         FHE.allow(_files[fileId].aesPassword, grantee);
-        
+
         emit FileGranted(fileId, msg.sender, grantee);
     }
 
@@ -104,10 +103,10 @@ contract SecretGallery is SepoliaConfig {
         require(_files[fileId].exists, "File does not exist");
         require(_files[fileId].owner == msg.sender, "Not file owner");
         require(_fileGrants[fileId][grantee], "Access not granted");
-        
+
         // Revoke access
         _fileGrants[fileId][grantee] = false;
-        
+
         // Remove from grantees list
         address[] storage grantees = _fileGrantees[fileId];
         for (uint256 i = 0; i < grantees.length; i++) {
@@ -117,7 +116,7 @@ contract SecretGallery is SepoliaConfig {
                 break;
             }
         }
-        
+
         emit FileGrantRevoked(fileId, msg.sender, grantee);
     }
 
@@ -127,11 +126,7 @@ contract SecretGallery is SepoliaConfig {
     /// @return aesPassword The encrypted AES password
     function getFileData(uint256 fileId) external view returns (euint256 ipfsHash, eaddress aesPassword) {
         require(_files[fileId].exists, "File does not exist");
-        require(
-            _files[fileId].owner == msg.sender || _fileGrants[fileId][msg.sender],
-            "Access denied"
-        );
-        
+
         return (_files[fileId].ipfsHashNumber, _files[fileId].aesPassword);
     }
 
@@ -141,11 +136,6 @@ contract SecretGallery is SepoliaConfig {
     /// @return timestamp The upload timestamp
     function getFileMetadata(uint256 fileId) external view returns (address owner, uint256 timestamp) {
         require(_files[fileId].exists, "File does not exist");
-        require(
-            _files[fileId].owner == msg.sender || _fileGrants[fileId][msg.sender],
-            "Access denied"
-        );
-        
         return (_files[fileId].owner, _files[fileId].timestamp);
     }
 
@@ -171,7 +161,7 @@ contract SecretGallery is SepoliaConfig {
     function getFileGrantees(uint256 fileId) external view returns (address[] memory) {
         require(_files[fileId].exists, "File does not exist");
         require(_files[fileId].owner == msg.sender, "Not file owner");
-        
+
         return _fileGrantees[fileId];
     }
 
